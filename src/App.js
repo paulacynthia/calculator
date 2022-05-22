eac9d4db1620652a67a1e3973bb102c94e6d21c2
paulacynthia <paulacynthia.alu.lmb@gmail.com>
2022-05-22 18:20:36 -0300

    Co-authored-by: Felipe Gomes <STL-FelipeGomes@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
 import './App.css';
+import { FormEvent, useState } from 'react';
+import { getValue } from '@testing-library/user-event/dist/utils';
 
 function App() {
+  function teste() {
+    console.log('cliquei');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="container">
+        <header>
+          <h1>Tabela Verdade Online</h1>
+          <input type="text" placeholder="Exemplo de expressão: pvq" />
+        </header>
+        <div class="buttons-especials">
+          <button type="button" value="AC">
+            AC
+          </button>
+          <button type="button" value="DEL">
+            DEL
+          </button>
+          <button type="button" value="=">
+            =
+          </button>
+        </div>
+
+        <div class="buttons">
+          <div>
+            <button type="button" value="p" onClick={teste}>
+              p
+            </button>
+            <button type="button" value="q">
+              q
+            </button>
+            <button type="button" value="r">
+              r
+            </button>
+            <button type="button" value="s">
+              s
+            </button>
+          </div>
+
+          <div>
+            <button type="button" value="~">
+              ~
+            </button>
+            <button type="button" value="^">
+              ^
+            </button>
+            <button type="button" value="v">
+              v
+            </button>
+            <button type="button" value="seta-direita">
+              ⇾
+            </button>
+          </div>
+          <div>
+            <button type="button" value="seta-dupla">
+              ⇿
+            </button>
+            <button type="button" value="+">
+              +
+            </button>
+            <button type="button" value="(">
+              (
+            </button>
+            <button type="button" value=")">
+              )
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
